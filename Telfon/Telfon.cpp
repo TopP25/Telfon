42ca607b19e1645b54938ca4b83a792bebc0ce74
acidhood999 <nazarok616@mail.ru>
2023-10-31 17:38:41 +0100

    ini4

--- Telfon/Telfon.cpp
+++ Telfon/Telfon.cpp
@@ -1,8 +1,14 @@
 ﻿
-
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(NULL, "RU");
+    ofstream file_out("Data.txt");
+    file_out << "Hello World!";
+    file_out.close();
+
 }
