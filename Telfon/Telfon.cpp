94bf70fcb1b9213cd2778cf61d663d0d1be5e752
Denis <denfill008@mail.ru>
2023-10-31 18:35:40 +0300

    doc_test

--- Telfon/Telfon.cpp
+++ Telfon/Telfon.cpp
@@ -11,10 +11,15 @@ int main()
     ofstream file_out;
     file_out.open("data.txt");
     file_out << "Hello Woradasld!";
-
+    
+    
     file_out.close();
     string str = "asdasdasd";
-    ifstream file_in("doc1.txt");
+
+    ifstream file_in;
+    file_in.open("doc1.txt");
+    
     file_in >> str;
     cout << str << endl;
+    file_in.close();
 }
