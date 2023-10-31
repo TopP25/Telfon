8ee7809f39d783c4be12c8f15ad0aaab2bf37dbe
Supchik <eagersupchik@gmail.com>
2023-10-31 18:26:47 +0300

    init

--- Telfon/Telfon.cpp
+++ Telfon/Telfon.cpp
@@ -8,7 +8,13 @@ int main()
 {
     ofstream file_out;
     file_out.open("data.txt");
-    file_out << "Hello World!";
+    if (!file_out.is_open())
+    {
+        cout << "error!!!" << endl;
+        return 0;
+    }
+    file_out << "Hello! World!";
     file_out.close();
-
+    string str;
+    ifstream file_in("data1.txt");
 }
