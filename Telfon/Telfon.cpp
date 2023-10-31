ce64a586044e9964b3e6360f226675e6e05ee673
Supchik <eagersupchik@gmail.com>
2023-10-31 18:53:33 +0300

    new init

--- Telfon/Telfon.cpp
+++ Telfon/Telfon.cpp
@@ -4,17 +4,17 @@
 #include <fstream>
 using namespace std;
 
+
 int main()
 {
     ofstream file_out;
     file_out.open("data.txt");
-    if (!file_out.is_open())
-    {
-        cout << "error!!!" << endl;
-        return 0;
-    }
-    file_out << "Hello! World!";
+    file_out << "Hello Word!";
     file_out.close();
     string str;
-    ifstream file_in("data1.txt");
+    ifstream file_in;
+    file_in.open("data1.txt");
+    file_in >> str;
+    cout << str << endl;
+    file_in.close();
 }
