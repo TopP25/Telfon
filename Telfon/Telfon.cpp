0d671217e31d8f6e96dcd1e584954e6a8def5b19
acidhood999 <nazarok616@mail.ru>
2023-10-31 17:39:56 +0100

    int5

--- Telfon/Telfon.cpp
+++ Telfon/Telfon.cpp
@@ -1,14 +1,46 @@
 ﻿
-#include <iostream>
-#include <fstream>
+#include "Stud.h"
 
 using namespace std;
 
+class Myclass
+{
+    vector<Stud> studens;
+
+public:
+    
+    Myclass(int i);
+    
+};
+
+Myclass::Myclass(int i)
+{
+    for(int i =0;)
+}
 int main()
 {
-    setlocale(NULL, "RU");
-    ofstream file_out("Data.txt");
+    /*setlocale(NULL, "RU");
+    ofstream file_out("Data1.txt");
+    if (!file_out.is_open())
+    {
+        cout << "error!!!" << endl;
+        return 0;
+    }
     file_out << "Hello World!";
     file_out.close();
 
+    string str;
+    
+    ifstream file_in("Data1.txt");
+
+    while (!file_in.eof())
+    {
+         file_in>>str;
+         cout << str<<' ';
+    }
+    file_in.close(); */
+
+   
+    //hello
+   
 }
