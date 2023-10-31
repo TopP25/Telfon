b939706550d5fa94c41b7146b4629a502c301e5c
Igor Putyajkin <putyajkin99@gmail.com>
2023-10-31 19:17:12 +0300

    commit 2

--- Telfon/Telfon.cpp
+++ Telfon/Telfon.cpp
@@ -2,13 +2,21 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main()
 {
     ofstream file_out;
-    file_out.open("data.txt");
-    file_out << "Hello World!";
+    file_out.open("data1.txt");
+    file_out << "high hello\ngign";
     file_out.close();
+    string str = "";
+    ifstream file_in("data1.txt");
+    while (!file_in.eof()) {
+        file_in >> str;
+        cout << str << " ";
+    }
+    file_in.close();
 
 }
