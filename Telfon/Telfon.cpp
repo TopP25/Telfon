4c0c44cd0112976738d50ef498800ebc5ad670d0
Denis <denfill008@mail.ru>
2023-10-31 20:10:22 +0300

    first test of saving

--- Telfon/Telfon.cpp
+++ Telfon/Telfon.cpp
@@ -5,13 +5,41 @@
 using namespace std;
 
 
-
+//void Save_in_file(Person a[], string name, int size)
+//{
+//    ofstream out;
+//    out.open(name);
+//    for (int i = 0; i <= 100; i++)
+//    {
+//        out << a[i].Print() + "|";
+//        
+//    }
+//    out.close();
+//
+//    string buf;
+//    ifstream in;
+//    in.open(name);
+//    while (!in.eof())
+//    {
+//        in >> buf;
+//        cout << buf << " ";
+//    }
+//    in.close();
+//
+//} need for test
 int main()
 {
+    //global Array for storaging person's information
+    Person Array[100]{};
+    int index = 0;
+    //__________________________________________
+    
     Person p("akshd", 123123,123123,123123,"dasdasd");
+    Array[0] = p;
     ofstream file_out;
     file_out.open("data.txt");
     file_out << p.Print();
+    file_out << "fgfgfgfgfgfgfg";
     file_out.close();
     
     
@@ -25,4 +53,6 @@ int main()
         cout << str<< " ";
     }
     file_in.close();
+
+ /*   Save_in_file(Array, "data.txt", 2);*/
 }
