0332ce9f2b81091e5423cdef84f951cf0b1ba0ea
Denis <denfill008@mail.ru>
2023-11-01 17:58:47 +0300

    save right

--- Telfon/Telfon.cpp
+++ Telfon/Telfon.cpp
@@ -4,38 +4,53 @@
 #include <fstream>
 using namespace std;
 
+Person Array[100]{};
+int index = 0;
 
-//void Save_in_file(Person a[], string name, int size)
-//{
-//    ofstream out;
-//    out.open(name);
-//    for (int i = 0; i <= 100; i++)
-//    {
-//        out << a[i].Print() + "|";
-//        
-//    }
-//    out.close();
-//
-//    string buf;
-//    ifstream in;
-//    in.open(name);
-//    while (!in.eof())
-//    {
-//        in >> buf;
-//        cout << buf << " ";
-//    }
-//    in.close();
-//
-//} need for test
+void new_person()
+{
+    string name1;
+    int t1;
+    int t2;
+    int t3;
+    string dop_inf_cin;
+    cin >> name1;
+    cin >> t1;
+    cin >> t2;
+    cin >> t3;
+    cin >> dop_inf_cin;
+    Person buf(name1,t1,t2,t3,dop_inf_cin+"|");
+    Array[index] = buf;
+    
+
+    cout << Array[index].Print() << endl;
+    
+
+
+    index++;
+}
+
+
+void save()
+{
+    string name_of_file;
+    cin >> name_of_file;
+    ofstream in;
+    in.open(name_of_file);
+    for (int i = 0; i < index; i++)
+    {
+        in << Array[i].Print();
+    }
+    in.close();
+}
 int main()
 {
     //global Array for storaging person's information
-    Person Array[100]{};
-    int index = 0;
+ 
     //__________________________________________
     
     Person p("akshd", 123123,123123,123123,"dasdasd");
-    Array[0] = p;
+    
     ofstream file_out;
     file_out.open("data.txt");
     file_out << p.Print();
@@ -50,9 +65,23 @@ int main()
     while (!file_in.eof())
     {
         file_in >> str;
-        cout << str<< " ";
+        cout << str<< " " << endl;
     }
     file_in.close();
+    system("pause");
+    system("cls");
 
- /*   Save_in_file(Array, "data.txt", 2);*/
+    string user_choice;
+    while (user_choice != "yes")
+    {
+        cin >> user_choice;
+        if (user_choice == "n")
+        {
+            new_person();
+        }
+        if (user_choice == "s")
+        {
+            save();
+        }
+    }
 }
